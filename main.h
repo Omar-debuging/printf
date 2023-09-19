ffa97b48b8e0c5e886934c00910df00ec76686bd
Omar-debuging <eladnaniomar153@gmail.com>
2023-09-19 13:29:05 -0700

    printf

--- main.h
+++ main.h
@@ -1,50 +1,45 @@
-#ifndef MAIN_H
-#define MAIN_H
-#define BUFFER_LEN 1024
+#ifndef WRITEFUNCTION
+#define WRITEFUNCTION
 
-#include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
-#include <stdlib.h>
 
+#define BUFFER_LEN 1024
 
 /**
- * struct specifier - struct token
- * @code: format
- * @f: function associated
+ * struct proto - ee
+ * @code: type
+ * @f: type
  *
  */
-typedef struct Specifier
+typedef struct proto
 {
 	char *code;
-	int (*f)(va_list arg);
-} specifier;
+	int (*f)();
+} proto;
 
-int _putchar(char c);
-int _puts(char *str);
+int _putchar(const char *c);
+int _putchar_val(const char c);
+int _writer(const char c);
 int _print_str(va_list arg);
-int _print_percent(va_list __attribute__((__unused__)) arg);
+int _print_percent(void);
 int _print_char(va_list arg);
 int _printf(const char *format, ...);
-int loopFunction(const char *format, specifier specifiers[], va_list args)
+int continueFunction(const char *format, va_list va, proto protos[]);
+int handle_args(const char *format, int *index, va_list args, proto protos[]);
 int _print_digit(va_list args);
-int _print_unsigned(unsigned int n);
+int _print_unsigned(va_list args);
 int _print_octal(va_list args);
 int _print_hex(va_list args);
 int _print_hex_maj(va_list args);
 int _reverse_print(const char *s);
+int _handle_print_unsigned_int(unsigned int digit, int writeCount);
 int _print_binary(va_list args);
-int _print_str_asc(va_list args);
+int _print_str_asc(va_list arg);
 int _print_str_rot(va_list arg);
-int _print_str_rev(va_list args);
+int _handle_print_hex(unsigned long decimalnum, char maj);
+int _print_str_rev(va_list arg);
 int _print_ptr(va_list arg);
-int pr_hexa_char(char c);
-char *int_to_binary(int n);
-char *arr_rev(char *arr);
-char *int_to_octal(unsigned int n);
-char *int_to_hex(int n);
-char *int_to_heX(int n);
-int base_len(int n, int dev);
-int _pr_int(int n);
+
 
 #endif
